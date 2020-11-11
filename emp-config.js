66ffd847977186ad24967a18b9468ccf8f8e8293
flyyuan <740004544@qq.com>
2020-11-11 17:12:01 +0800

    feat: 更新应用模板

--- emp-config.js
+++ emp-config.js
@@ -1,67 +1,52 @@
+const withVue3 = require('@efox/emp-vue3')
 const path = require('path')
-const {VueLoaderPlugin} = require('vue-loader')
-//
 const ProjectRootPath = path.resolve('./')
-// const packagePath = path.join(ProjectRootPath, 'package.json')
-// const {dependencies} = require(packagePath)
-//
-const {getConfig} = require(path.join(ProjectRootPath, './src/config'))
-//
-module.exports = ({config, env, empEnv}) => {
+const { getConfig } = require(path.join(ProjectRootPath, './src/config'))
+module.exports = withVue3(({ config, env, empEnv }) => {
   const confEnv = env === 'production' ? 'prod' : 'dev'
   const conf = getConfig(empEnv || confEnv)
-  console.log('config', conf)
-  //
-  const srcPath = path.resolve('./src')
-  config.entry('index').clear().add(path.join(srcPath, 'main.js'))
-  //
-  config.resolve.alias.set('vue', '@vue/runtime-dom')
-  config.plugin('vue3').use(VueLoaderPlugin, [])
-  config.module
-    .rule('vue')
-    .test(/\.vue$/)
-    .use('vue-loader')
-    .loader('vue-loader')
-  //
-  const host = conf.host
   const port = conf.port
-  const projectName = 'vue3Components'
+  const projectName = 'vue3Project'
   const publicPath = conf.publicPath
+  // 设置项目URL
   config.output.publicPath(publicPath)
+  // 设置项目端口
   config.devServer.port(port)
-  //
   config.plugin('mf').tap(args => {
     args[0] = {
       ...args[0],
       ...{
+        // 项目名称
         name: projectName,
-        library: {type: 'var', name: projectName},
+        // 暴露项目的全局变量名
+        library: { type: 'var', name: projectName },
+        // 被远程引入的文件名
         filename: 'emp.js',
-        /* remotes: {
+        remotes: {
+          // 远程项目别名:远程引入的项目名
           vue3Components: 'vue3Components',
-        }, */
-        exposes: {
-          './Content': './src/components/Content',
-          './Button': './src/components/Button',
         },
-        /* shared: {
-          ...dependencies,
-        }, */
+        // 需要暴露的东西
+        exposes: {
+          // 别名:组件的路径
+        },
       },
     }
     return args
   })
-  //
+  // 配置 index.html
   config.plugin('html').tap(args => {
     args[0] = {
       ...args[0],
       ...{
-        title: 'EMP Vue3 Components',
+        // head 的 title
+        title: 'EMP Vue3 Project',
+        // 远程调用项目的文件链接
         files: {
-          // js: ['http://localhost:8005/emp.js'],
+          js: [conf.baseRemoteEntry],
         },
       },
     }
     return args
   })
-}
+})
--- src/config.js
+++ src/config.js
@@ -1,13 +1,17 @@
 const env = process.env.EMP_ENV || 'dev'
 const dev = {
   host: 'localhost',
-  port: 8006,
-  publicPath: 'http://localhost:8006/',
+  port: 8005,
+  publicPath: 'http://localhost:8005/',
+  baseRemote: 'http://localhost:8006',
+  baseRemoteEntry: `http://localhost:8006/emp.js`,
 }
 const prod = {
   host: 'localhost',
-  port: 8006,
-  publicPath: 'http://localhost:8006/',
+  port: 8005,
+  publicPath: 'http://localhost:8005/',
+  baseRemote: 'http://localhost:8006',
+  baseRemoteEntry: `http://localhost:8006/emp.js`,
 }
 const configs = {dev, prod}
 exports.getConfig = env => {
